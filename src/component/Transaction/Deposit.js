e6710d61e496be7dab1fab363d7a7baa0f4a7fbc
son-vo-cs <sonvo.cs@gmail.com>
2020-05-10 23:46:37 -0700

    updates

--- src/component/Transaction/Deposit.js
+++ src/component/Transaction/Deposit.js
@@ -95,7 +95,7 @@ class Deposit extends React.Component  {
 
                 <div>
                 <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-				<Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+				<Dropdown.Item style={{color: "red"}} href="#/action-1">Action</Dropdown.Item>
 				<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
 				<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
 			</DropdownButton>
