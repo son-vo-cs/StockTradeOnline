858d4b395e95f70a579023ab430e238e169ae8b6
son-vo-cs <sonvo.cs@gmail.com>
2020-06-05 19:50:54 -0700

    updates

---
+++ src/component/Stock/PageStock.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import Modal from '@material-ui/core/Modal';
+import Stock from './Stock'
+import { Redirect } from 'react-router-dom';
+
+class PageStock extends React.Component  {
+
+    constructor(props)
+    {
+        super(props);
+        this.state = 
+        {
+            
+            
+        }
+    }
+  
+
+
+   
+    render(){
+        return (
+
+            <div>
+            {/* // <div  style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}> */}
+              {/* <Stock option={option}></Stock> */}
+              {/* <SearchBar argument={aa} user={user}></SearchBar> */}
+              {/* <Home user={user} account={account} performs={performs}></Home> */}
+          </div>
+          );
+    }
+}
+
+
+export default PageStock;
+
